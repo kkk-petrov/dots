d244fb2ca926c36abfa234ce279370fcc4895f53
kkk-petrov <kkpetrov.dev@gmail.com>
2024-01-21 14:05:08 +0200

    update hyprland

---
+++ .config/hypr/scripts/setgtk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
+gsettings set org.gnome.desktop.interface cursor-theme 'Adwaita'
+gsettings set org.gnome.desktop.interface font-name 'JetBrains Mono 10'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'

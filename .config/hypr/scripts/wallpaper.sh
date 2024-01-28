e7c386fdf872c7451427172a1c6e1259455953e2
kkk-petrov <kkpetrov.dev@gmail.com>
2024-01-28 22:57:37 +0200

    update: waybar; fix: init swww on startup

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 WALLPAPER_DARK="$HOME/Wallpapers/wallpaper3.jpg"
 WALLPAPER_LIGHT="$HOME/Wallpapers/wallpaper2.jpg"
+wallpaper=""
 
 if gsettings get org.gnome.desktop.interface gtk-theme | grep -q "dark"; then
-	swww img $WALLPAPER_DARK
+	wallpaper="${WALLPAPER_DARK}"
 else
-	swww img $WALLPAPER_LIGHT
+	wallpaper="${WALLPAPER_LIGHT}"
 fi
+
+# swww query
+# if [ $? -eq 1 ]; then
+# fi
+#
+
+# swww init
+swww img $wallpaper
